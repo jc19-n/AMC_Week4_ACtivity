ef270a75e349d4e004c40efc44ba6cebd3b67d4a
jc19-n <jereylchervennebril@gmail.com>
2025-02-06 13:27:16 +0800

    "ScrollView"

---
+++ Activity1/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {StyleSheet, Text, ScrollView, StatusBar} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+
+const App = () => (
+  <SafeAreaProvider>
+    <SafeAreaView>
+      <ScrollView>
+        <Text>
+          JEJEJEJEJEJEJEJEsdsdgdfgsdscwefggergegergerggargsdfgdfgd
+        </Text>
+      </ScrollView>
+    </SafeAreaView>
+  </SafeAreaProvider>
+);
+
+
+export default App;
